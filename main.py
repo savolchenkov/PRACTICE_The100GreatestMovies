e380db6d88027f4411cb062f56f3c475a0a58154
savolchenkov <sergeivolchenkov91@gmail.com>
2023-03-15 19:14:09 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+soup.find_all(name="h3", class_="title")
